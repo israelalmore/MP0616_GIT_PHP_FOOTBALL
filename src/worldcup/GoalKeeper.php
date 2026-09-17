e791204aa105830e694ea7125f61b4a3435793d9
israelalmore <israelalmore@gmail.com>
2026-09-17 16:31:17 +0200

    Goalkepper is okay

--- src/worldcup/GoalKeeper.php
+++ src/worldcup/GoalKeeper.php
@@ -2,27 +2,34 @@
 
 namespace WorldCup;
 
+require_once __DIR__ . '/Ball.php';
+require_once __DIR__ . '/Player.php';
+
 /**
  * Class to define the goalkeeper
  */
-class Goalkeeper extends Player {
-    public $globes; // property declared without type, like in original
+class GoalKeeper extends Player
+{
+    public bool $globes; // property declared without type, like in original
 
     /**
      * Get the globes
      */
-    public function isGlobes() {
+    public function isGlobes()
+    {
         return $this->globes;
     }
 
     /**
      * Set the globes
      */
-    public function setGlobes($globes) {
+    public function setGlobes(bool $globes)
+    {
         $this->globes = $globes;
     }
 
-    public function block(Ball $ball) {
+    public function block(Ball $ball)
+    {
         $effects = ["with success", "without success"];
 
         $effect = $effects[array_rand($effects)];
